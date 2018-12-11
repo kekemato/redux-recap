eb7a7f861e03ff0205f8345f78215fc7dc148c93
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-12-11 10:56:09 +0100

    task 13 - rendering counter

---
+++ src/Counter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Counter = props => (
+    <div>
+        <h1></h1>
+        <button>+</button>
+        <button>-</button>
+    </div>
+)
+
+export default Counter
