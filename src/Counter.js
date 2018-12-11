117b365566e786c07dff67f35e5d07ecfaacbbc6
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-12-11 11:07:29 +0100

    task 14 - working counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { inc, dec } from './state/counter'
 
 const Counter = props => (
     <div>
-        <h1></h1>
-        <button>+</button>
-        <button>-</button>
+        <h1>{props._counterValue}</h1>
+        <button
+            onClick={props._inc}
+        >
+            +
+        </button>
+        <button
+            onClick={props._dec}
+        >
+            -
+        </button>
     </div>
 )
 
-export default Counter
+const mapStateToProps = state => ({
+    _counterValue: state.counterReducerName.counter
+})
+
+const mapDispatchToProps = dispatch => ({
+    _inc: () => dispatch(inc()),
+    _dec: () => dispatch(dec())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
