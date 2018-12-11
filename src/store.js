1658f8e3055b4660de774da76297cb02c79d542d
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-12-11 10:11:13 +0100

    made first action

--- src/store.js
+++ src/store.js
@@ -1,17 +1,39 @@
 import { createStore } from 'redux'
 
-export const store = createStore(
-    () => ({
-        people: {
-            person1: {
-                name: 'Ala'
-            },
-            person2: {
-                name: 'Bartek'
-            }
+const FIRST_ACTION = 'FIRST_ACTION'
+
+const INITIAL_STATE = {
+    people: {
+        person1: {
+            name: 'Ala'
+        },
+        person2: {
+            name: 'Bartek'
         }
-    }),
+    }
+}
+
+const reducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case FIRST_ACTION:
+            return {
+                ...state,
+                firstAction: true
+            }
+        default:
+            return {
+                ...state,
+                firstAction: false
+            }
+    }
+}
+
+export const store = createStore(
+    reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
     window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
+store.dispatch({
+    type: FIRST_ACTION
+})
