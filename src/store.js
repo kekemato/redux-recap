c9cecbd754e1466c2c4719aaf7607297d00b9a30
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-12-11 09:54:50 +0100

    creating example reducer with state only

--- src/store.js
+++ src/store.js
@@ -1,7 +1,16 @@
 import { createStore } from 'redux'
 
 export const store = createStore(
-    () => { },
+    () => ({
+        people: {
+            person1: {
+                name: 'Ala'
+            },
+            person2: {
+                name: 'Bartek'
+            }
+        }
+    }),
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
     window.__REDUX_DEVTOOLS_EXTENSION__()
 )
